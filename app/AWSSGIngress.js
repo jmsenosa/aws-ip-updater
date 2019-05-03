bd1f5a4db77983e5ab38ad8acc118494f9e56f08
JM Senosa <jmsenosa@gmail.com>
2019-05-03 19:14:15 +0800

    update this

--- app/AWSSGIngress.js
+++ app/AWSSGIngress.js
@@ -12,22 +12,65 @@ class AWSSGIngress {
     this.sgID.push(sgID);
     this.ec2 = new AWS.EC2(); 
   }
+ 
 
-  getSecurityGroups() {
-    this.ec2.describeSecurityGroups({
-      GroupIds: this.sgID
-    }, (err, data) => {
-      if (err) {
-        console.log("Error", err);
-      } else {
-        console.log(data);
+  getSecurityGroups () {
+
+    return new Promise((resolve, reject) => {
+      this.ec2.describeSecurityGroups({
+        GroupIds: this.sgID
+      }, (err, data) => {
+  
+        if (err === null) {
+          resolve(data);
+        } else { 
+          reject(err);
+        }
+      });
+    });
+    
+  }
+  
+  authSecurityGroupIngress (IpPermission) {
+    return new Promise((resolve, reject) => {
+      this.ec2.authorizeSecurityGroupIngress({ 
+        GroupIds: this.sgID,
+        IpPermissions: IpPermission
+      }, (err, data) => {
+        if (err === null) {
+          resolve(data);
+        } else { 
+          reject(err);
+        }
+      });
+    });
+  }
+
+  revokeSecurityGroupIngress (IpPermission) {
+    return new Promise((resolve, reject) => {
+      
+      if (IpPermission == "") {
+        reject("IpPermission is blank!");
       }
+
+      var param = {
+        GroupIds: this.sgID,
+        IpPermissions: IpPermission
+      }; 
+
+      this.ec2.revokeSecurityGroupIngress(param, (err, data) => { 
+        if (err === null) {
+          resolve(data);
+        } else { 
+          reject(err);
+        }
+      });
     });
   }
 
 
 } 
 
-// module.exports = AWSSGIngress;
-
 module.exports = AWSSGIngress;
+
+// module.exports = AWSSGIngress;
--- app/index.js
+++ app/index.js
@@ -3,6 +3,9 @@
 // 120.29.112.224/32 
 
 var sgID = 'sg-bf6f40c7';
+var sgIPLabel = "HKZ Office";
+
+
 var ip = require('whatismyip');
 var options = {
   url: 'http://checkip.dyndns.org/',
@@ -15,8 +18,42 @@ var options = {
 ip.whatismyip(options, (err, ipdata) => {
   if (err === null) { 
  
-    var AWSSecurityGroupIngress = new AWSSGIngress(ipdata.ip, sgID);
+    var awsSGI = new AWSSGIngress(ipdata.ip, sgID);
+    awsSGI.getSecurityGroups()
+    .then((data) => {
+      var iprangePermssion = [
+        {
+          FromPort: 22, 
+          IpProtocol: "tcp", 
+          IpRanges: [],
+          ToPort: 22
+        }
+      ];
 
-    AWSSecurityGroupIngress.getSecurityGroups();
+      data.SecurityGroups.filter((securityGroup) => {
+        // console.log(securityGroup);
+        if (securityGroup.GroupId == sgID) {
+          return securityGroup.IpPermissions.filter((ip, i) => {
+            if (ip.FromPort == 22) {
+              var ipp = ip.IpRanges.filter((iprange, int) => {
+                if (iprange.Description == sgIPLabel) {
+                  iprangePermssion.IpRanges = iprange;
+                  return iprange;
+                }
+              });  
+            }
+          });
+        }
+      });
+      
+      awsSGI.revokeSecurityGroupIngress(iprangePermssion)
+      .then((data) => {
+        
+      });
+
+    })
+    .catch((err) => {
+      console.log(err);
+    }); 
   }
 });
